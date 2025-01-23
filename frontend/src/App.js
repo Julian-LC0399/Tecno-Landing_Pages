96a25531affe66499b7ed4b4c2f113b1320c73e5
Julián <julianlopezcastillo12@gmail.com>
2025-01-23 01:40:44 -0400

    y

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Chatbot from './components/Chatbot';
 import PhoneDetails from './components/PhoneDetails'; 
 import camon19Image from './images/camon19.png'; // Importa la imagen
 import spark8Image from './images/spark8.jpg'; // Importa la imagen
+import spark30Image from './images/spark30.png'
 import './style.css';
 
 // Definición de la lista de teléfonos
@@ -15,8 +16,10 @@ const phones = [
         description: (
             <div>
                 <p><strong>Cámara:</strong> Alta resolución con IA para fotos de calidad.</p>
-                <p><strong>Pantalla:</strong> 6.8 pulgadas, ideal para multimedia.</p>
+                <p><strong>Pantalla:</strong> 6.8 pulgadas.</p>
                 <p><strong>Batería:</strong> Duradera, perfecta para uso diario.</p>
+                <p><strong>Almacenamiento:</strong> 128GB.</p>
+                <p><strong>Ram:</strong> 6GB.</p>
             </div>
         ),
         price: 199.99,
@@ -27,12 +30,29 @@ const phones = [
         description: (
             <div>
                 <p><strong>Cámara:</strong> Doble cámara para capturar momentos.</p>
-                <p><strong>Pantalla:</strong> 6.5 pulgadas, buena para juegos y videos.</p>
+                <p><strong>Pantalla:</strong> 6.5 pulgadas.</p>
                 <p><strong>Batería:</strong> Duradera, perfecta para uso diario.</p>
+                <p><strong>Almacenamiento:</strong> 64GB.</p>
+                <p><strong>Ram:</strong> 2GB.</p>
             </div>
         ),
         price: 149.99,
     },
+    {
+        model: 'Tecno Spark 30',
+        image: spark30Image,
+        description: (
+            <div>
+                <p><strong>Cámara:</strong> Tres cámaras para capturar momentos.</p>
+                <p><strong>Pantalla:</strong> 6.7 pulgadas</p>
+                <p><strong>Batería:</strong> Duradera, perfecta para uso diario</p>
+                <p><strong>Almacenamiento:</strong> 256GB.</p>
+                <p><strong>Ram:</strong> 8GB.</p>
+            </div>
+        ),
+        price: 179.99,
+
+    }
 ];
 
 const App = () => {
--- frontend/src/components/Chatbot.jsx
+++ frontend/src/components/Chatbot.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import camon19Image from '../images/camon19.png'; // Asegúrate de que la ruta sea correcta
 import spark8Image from '../images/spark8.jpg'; // Asegúrate de que la ruta sea correcta
+import spark30Image from '../images/spark30.png'; // Asegúrate de que la ruta sea correcta
 
 const phones = [
     {
@@ -9,8 +10,10 @@ const phones = [
         description: (
             <div>
                 <p><strong>Cámara:</strong> Alta resolución con IA para fotos de calidad.</p>
-                <p><strong>Pantalla:</strong> 6.8 pulgadas, ideal para multimedia.</p>
+                <p><strong>Pantalla:</strong> 6.8 pulgadas.</p>
                 <p><strong>Batería:</strong> Duradera, perfecta para uso diario.</p>
+                <p><strong>Almacenamiento:</strong> 128GB.</p>
+                <p><strong>Ram:</strong> 6GB.</p>
             </div>
         ),
         price: 199.99,
@@ -21,12 +24,28 @@ const phones = [
         description: (
             <div>
                 <p><strong>Cámara:</strong> Doble cámara para capturar momentos.</p>
-                <p><strong>Pantalla:</strong> 6.5 pulgadas, buena para juegos y videos.</p>
+                <p><strong>Pantalla:</strong> 6.5 pulgadas.</p>
                 <p><strong>Batería:</strong> Duradera, perfecta para uso diario.</p>
+                <p><strong>Almacenamiento:</strong> 64GB.</p>
+                <p><strong>Ram:</strong> 2GB.</p>
             </div>
         ),
         price: 149.99,
     },
+    {
+        model: 'Tecno Spark 30',
+        image: spark30Image,
+        description: (
+            <div>
+               <p><strong>Cámara:</strong> Tres cámaras para capturar momentos.</p>
+               <p><strong>Pantalla:</strong> 6.7 pulgadas</p>
+                <p><strong>Batería:</strong> Duradera, perfecta para uso diario</p>
+                <p><strong>Almacenamiento:</strong> 256GB.</p>
+                <p><strong>Ram:</strong> 8GB.</p>
+            </div>
+        ),
+        price: 179.99,
+    }
 ];
 
 const Chatbot = () => {
@@ -68,6 +87,15 @@ const Chatbot = () => {
                     <p><strong>Precio:</strong> ${phones[1].price}</p>
                 </div>
             );
+            } else if (lowerMessage.includes('spark 30')) {
+                return (
+                    <div>
+                        <h3>{phones[2].model}</h3>
+                        <img src={phones[2].image} alt={phones[2].model} style={{ width: '100px' }} />
+                        {phones[2].description}
+                        <p><strong>Precio:</strong> ${phones[2].price}</p>
+                    </div>
+                );
         } else {
             return "Lo siento, no entiendo tu pregunta. ¿Puedes reformularla?";
         }
